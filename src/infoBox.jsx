7342dc372634456853abe0704ef4846489c3a8d4
Saurabh Anand <saurabhckhekamtaul@gmail.com>
2025-05-14 09:02:01 +0530

    Modification in some part

--- src/infoBox.jsx
+++ src/infoBox.jsx
@@ -2,7 +2,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import "./InfoBox.css";
+import "./infoBox.css";
 
 export default function InfoBox({ info }) {
 
